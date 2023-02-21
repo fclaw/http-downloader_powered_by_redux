44c7c01c40f4cac8e98f8d64ea3ca4f7d1489be0
Sonny <sonny@Sonny.local>
2023-02-21 17:13:03 +0300

    init

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") {}
+, nodejs ? pkgs."nodejs-18_x"
+ }:
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "11d3bd58ce6e32703bf69cec04dc7c38eabe14ba";
+      sha256 = "sha256-tESal32bcqqdZO+aKnBzc1GoL2mtnaDtj2y7ociCRGA=";
+    }) { inherit pkgs; };
+  new_spago = pkgs.spago.overrideAttrs (old: {
+        name = "spago";
+        src = pkgs.fetchgit {
+          url = "https://github.com/purescript/spago.git";
+          rev = "d16d4914200783fbd820ba89dbdf67270454faf5";
+          sha256 = "sha256-MMKt5BWpdvKxGlLB/5TkFEKXODUIHQL5T21wtc/DbQM=";
+        }; });
+in
+  pkgs.mkShell {
+    buildInputs = [
+      nodejs
+      new_spago
+      easy-ps.purs-0_15_7
+      easy-ps.psc-package
+      easy-ps.purescript-language-server
+      easy-ps.purs-backend-es ];}
