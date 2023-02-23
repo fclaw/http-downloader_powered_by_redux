f1af376770b905097bf52729060d0a855a6daea0
Sonny <sonny@Sonny.local>
2023-02-23 18:52:34 +0300

    cancel req

--- src/model/HttpDownloader/HttpDownloader.js
+++ src/model/HttpDownloader/HttpDownloader.js
@@ -4,10 +4,12 @@ import
   {   selectUrl
     , selectState
     , selectContent
+    , selectSignalKey
     , HttpDownLoaderState
     , fillUrl
     , cancel
     , downloadPage
+    , cancelPage
   } from './httpdownloaderSlice'
 
 export function HttpDownloader() {
@@ -15,12 +17,13 @@ export function HttpDownloader() {
   const ct = useSelector(selectContent)
   const st = useSelector(selectState)
   const url = useSelector(selectUrl)
+  const sig = useSelector(selectSignalKey)
   return (
     <div>
         <input type="text" onInput={evt => dispatch(fillUrl(evt.target.value))} />
       <div class="btn-group" role="group" aria-label="...">
-        <button type="button" class="btn btn-default" disabled={st == HttpDownLoaderState[1]} onClick={() => dispatch(downloadPage(url))} >start</button>
-        <button type="button" class="btn btn-default" disabled={st == HttpDownLoaderState[0]} onClick={() => dispatch(cancel())}>stop</button>
+        <button type="button" class="btn btn-default" disabled={st == HttpDownLoaderState[1]} onClick={() => dispatch(downloadPage({urlf: url, sigf: sig}))} >start</button>
+        <button type="button" class="btn btn-default" disabled={st == HttpDownLoaderState[0]} onClick={() => dispatch(cancelPage(sig))}>stop</button>
       </div>
       <p>{st == HttpDownLoaderState[1] ? "download is in process ..." : ct}</p>
     </div>
--- src/model/HttpDownloader/httpdownloaderSlice.js
+++ src/model/HttpDownloader/httpdownloaderSlice.js
@@ -2,19 +2,46 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 export const HttpDownLoaderState = ["Idle", "InProgress"];
 
+const ABORT_REQUEST_CONTROLLERS = new Map();
+
+function abortAndGetSignal(key) {
+   const newController = new AbortController();
+   ABORT_REQUEST_CONTROLLERS.set(key, newController);
+   return newController.signal;
+ }
+
+function abortRequest(key) {
+   const controller = ABORT_REQUEST_CONTROLLERS.get(key);
+   controller.abort();
+}
+
+function signalKeyGen(length) {
+   let result = '';
+   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   const charactersLength = characters.length;
+   let counter = 0;
+   while (counter < length) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     counter += 1;
+   }
+   return result;
+}
+
 const initialState =
  {
     url: null,
     state: HttpDownLoaderState[0],
-    content: null
+    content: null,
+    signalKey: null
  }
 
 export const downloadPage =
  createAsyncThunk(
   'httpdownloader/start',
-  async (url) => {
+  async (data) => {
    // 'https://elm-lang.org/assets/public-opinion.txt'
-   const res = await fetch(url.payload);
+   let sig = abortAndGetSignal(data.sigf)
+   const res = await fetch(data.urlf.payload, { signal: sig });
    const buffer = await streamToArrayBuffer(res.body);
    const text = new TextDecoder().decode(buffer);
    return text;
@@ -37,22 +64,36 @@ async function streamToArrayBuffer(stream: ReadableStream<Uint8Array>): Promise<
    return result;
 }
 
+export const cancelPage =
+ createAsyncThunk(
+   'httpdownloader/cancel',
+   async (signalKey) => {
+      abortRequest(signalKey);
+      return  signalKey;
+   })
+
 const httpdownloaderSlice = createSlice({
     name: 'httpdownloader',
     initialState,
     reducers: {
-      fillUrl: fillUrlBody,
-      cancel: cancelBody
+      fillUrl: fillUrlBody
     },
     extraReducers: builder => {
       builder
         .addCase(downloadPage.pending, (state, action) => { downloadPagePendingBody(state); })
         .addCase(downloadPage.fulfilled, (state, action) => { downloadPageFulfilledBody(state, action.payload); })
-        .addCase(downloadPage.rejected, (state, action) => { downloadPageRejectedBody(state); });
+        .addCase(downloadPage.rejected, (state, action) => { downloadPageRejectedBody(state); })
+        .addCase(cancelPage.pending, (state, action) => { state })
+        .addCase(cancelPage.fulfilled, (state, action) => { cancelBody(state); })
+        .addCase(cancelPage.rejected, (state, action) => { state });
     }
   })
 
-function fillUrlBody(state, url) { state.url = url }
+function fillUrlBody(state, url)
+  {
+    state.url = url;
+    state.signalKey = signalKeyGen(20);
+  }
 
 function downloadPagePendingBody(state) {
    state.content = null;
@@ -76,8 +117,9 @@ function cancelBody(state) {
 
 export default httpdownloaderSlice.reducer
 
-export const { cancel, fillUrl } = httpdownloaderSlice.actions
+export const { fillUrl } = httpdownloaderSlice.actions
 
 export const selectUrl = state => state.httpdownloader.url
 export const selectState = state => state.httpdownloader.state
 export const selectContent = state => state.httpdownloader.content
+export const selectSignalKey = state => state.httpdownloader.signalKey
